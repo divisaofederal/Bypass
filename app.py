3eb8a3711a24e8e70159d6ff99ea0da78b3b7c11
divisaofederal <166842966+divisaofederal@users.noreply.github.com>
2024-04-14 00:19:55 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 # Configurando as opções do Chrome para headless
 options = webdriver.ChromeOptions()
@@ -33,13 +34,11 @@ WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "bu
 # Enviando o formulário de login
 driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-# Aguardando o login ser concluído
-WebDriverWait(driver, 10).until(EC.url_contains("instagram.com/accounts/onetap"))
-
-# Verificando se o login foi bem sucedido
-if "instagram.com/accounts/onetap" in driver.current_url:
+try:
+    # Aguardando a presença do texto "Salvar suas informações de login?"
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(text(), 'Salvar suas informações de login?')]")))
     print("Login bem sucedido!")
-else:
+except TimeoutException:
     print("Falha no login. Verifique suas credenciais.")
 
 # Finalizando o driver
